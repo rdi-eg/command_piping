4413b8ac00170cbb97a587279828b85d8262c563
Adham Zahran <adham.zahran@hotmail.com>
2018-04-12 15:04:47 +0200

    code janitor passed by

--- command_piping.cpp
+++ command_piping.cpp
@@ -1,18 +1,23 @@
 #include "rdi_command_piping.h"
 #include<string>
-using namespace std;
 
+using namespace std;
 
 string RDI::execute_command(const string& cmd)
 {
-	redi::ipstream proc(cmd, redi::pstreams::pstdout|redi::pstreams::pstderr);
-	string line, result ="";
+	redi::ipstream proc(cmd, redi::pstreams::pstdout | redi::pstreams::pstderr);
+	string line, result = "";
+
 	while(getline(proc.out(), line))
 	{
 	   if(result.size() > 0)
-		   result+="\n";
-	   result+=line;
+	   {
+		   result += '\n';
+	   }
+
+	   result += line;
 	}
+
 	return result;
 }
 
--- rdi_command_piping.h
+++ rdi_command_piping.h
@@ -1,11 +1,14 @@
 #ifndef COMMAND_PIPING_H
 #define COMMAND_PIPING_H
 
-
 #include <string>
 #include <pstream.h>
-namespace RDI {
+
+namespace RDI
+{
+
 std::string execute_command(const std::string& cmd);
-}
+
+} // namespace RDI
 
 #endif // COMMAND_PIPING_H
